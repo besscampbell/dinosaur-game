7d1de4893047ea9b3c25c04a71c58ee50ffbbb6b
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-17 11:44:03 -0800

    working functionality to main.js
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- src/dino-service.js
+++ src/dino-service.js
@@ -1,15 +1,17 @@
 export default class DinoAPI {
   static getName() {
-    return new Promise(function(resolve,reject) {
+    return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`;
+      const url = "http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1";
       request.onload = function(){
         if (this.status === 200) {
+          console.log("resolve");
           resolve(request.response);
         } else {
+          console.log("reject");
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import DinoAPI from './dino-service.js'
+import DinoAPI from './dino-service.js';
 
 function clearFields() {
   $("#letterGuess").val("");
 }
 
 $(document).ready(function() {
-  $('#guessButton').submit(function() {
-    let letterGuess = $('#letterGuess').val;
-    clearFields();
+  $('#getWord').click(function() {  
     let promise = DinoAPI.getName();
+    console.log(promise);
     promise.then(function(response) {
       const body = JSON.parse(response);
+      console.log(`${body[0][0]}`);
       $('#dinoWord').text(`${body[0][0]}`);
-      $("#letter").text(`You guessed ${letterGuess}`);
     }, function(error) {
       $("#error").text(`There was an error processing your request: ${error}`);
-    })
-  })
-})
+    });
+  });  
+  $('#userGuesses').submit(function(event) {
+    event.preventDefault();
+    let letterGuess = $('#letterGuess').val();
+    $("#letter").text(`You guessed ${letterGuess}`);
+    clearFields();
+  });
+});
